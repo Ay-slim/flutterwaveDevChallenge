24c1999b92fd3e91a4b3188b34065afb65c17638
oluwaferanmiadetunji <oluwaferanmiadetunji@gmail.com>
2021-01-09 19:56:23 +0100

    .

--- client/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import Routes from './components/routes';
-
-function App(): JSX.Element {
-    return <Routes />;
-}
-
-export default App;
--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,3 +1,15 @@
-const Home = (): JSX.Element => <div>Home</div>;
+import './style.scss';
+
+const Home = (): JSX.Element => (
+    <div className="home__container">
+        <div className="text__container">
+            <p className="about__text">The Easiest way to get your products</p>
+            <button className="login__redirect">Start Now</button>
+        </div>
+        <div className="image__container">
+            <p className="about__text">The Easiest way to get your products</p>
+        </div>
+    </div>
+);
 
 export default Home;
